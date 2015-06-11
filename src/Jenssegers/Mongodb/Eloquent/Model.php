eaef109979b812cea73eee759f9cb0fa18944498
Paul Rysiavets <paul@afrogleap.com>
2015-06-11 16:29:44 +0200

    NaN is pre-converted to 0

--- src/Jenssegers/Mongodb/Eloquent/Model.php
+++ src/Jenssegers/Mongodb/Eloquent/Model.php
@@ -325,6 +325,12 @@ abstract class Model extends BaseModel {
             {
                 $value = (string) $this->asDateTime($value);
             }
+
+            //if (is_nan($value) || is_infinite($value))
+            if (is_nan($value))
+            {
+                $value = 0;
+            }
         });
         /* FIXME: it messes with parameters
          foreach ($this->getDates() as $key) {
